943c56e2b2c4473170fb097684b4405d64f99348
Tej Chajed <tej@theorem.dev>
2026-07-28 15:48:34 -0700

    Remove output from agents-awake

--- fish/functions/agents-awake.fish
+++ fish/functions/agents-awake.fish
@@ -1,4 +1,3 @@
 function agents-awake --description 'Prevent idle system sleep while agents are running'
-    echo 'Keeping this Mac awake for agents; press Ctrl-C to stop.'
     command caffeinate -i $argv
 end
