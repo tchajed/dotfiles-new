295b0e29def2ad4cd87656f02b7719fd51975597
Tej Chajed <tchajed@gmail.com>
2026-08-15 09:06:08 -0700

    Disable diagnostics in Markdown buffers

--- vim/lazyvim-nvim/lua/config/autocmds.lua
+++ vim/lazyvim-nvim/lua/config/autocmds.lua
@@ -6,3 +6,12 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("markdown_diagnostics", { clear = true }),
+  pattern = "markdown",
+  callback = function(args)
+    vim.diagnostic.enable(false, { bufnr = args.buf })
+  end,
+  desc = "Disable diagnostics by default in Markdown buffers",
+})
